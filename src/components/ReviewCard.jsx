53b2ea3f505d968ff5ec340983ea0af166778fdb
Abderahmane Thimbo <abderahmanethimbo@gmail.com>
2025-01-14 20:17:24 +0300

    Review

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -8,11 +8,11 @@ const ratings = Array.from({ length: 5 }, () => ({
 
 const ReviewCard = ({ content, imgSrc, name, company }) => {
     return (
-        <div className="">
-            <div className="">
+        <div className="bg-zinc-800 p-5 rounded-xl min-w-[320px] flex flex-col lg:min-w-[420px]">
+            <div className="felx items-center gap-1 mb-3">
                 {
                     ratings.map(({ icon, style }, key) => (
-                        <span className="material-symbols-rounded"
+                        <span className="material-symbols-rounded text-yellow-300 text-[18px]"
                             key={key}
                             style={style}>
                             {icon}
@@ -21,12 +21,12 @@ const ReviewCard = ({ content, imgSrc, name, company }) => {
                 }
             </div>
 
-            <p className="">
+            <p className="text-zinc-400 mb-8">
                 {content}
             </p>
 
-            <div className="">
-                <figure className="">
+            <div className="flex items-center gap-2 mt-auto">
+                <figure className="img-box rounded-lg">
                     <img
                         src={imgSrc}
                         alt={name}
@@ -39,7 +39,7 @@ const ReviewCard = ({ content, imgSrc, name, company }) => {
 
                 <div>
                     <p>{name}</p>
-                    <p className="">
+                    <p className="text-xs text-zinc-400 tracking-wider">
                         {company}
                     </p>
                 </div>
