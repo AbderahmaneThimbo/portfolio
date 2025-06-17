b98c215734a89c6d028ee53dc7d2d6ca88a70d97
AbderahmaneThimbo <167882342+AbderahmaneThimbo@users.noreply.github.com>
2025-06-17 22:21:17 +0000

    remove reviews link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
               <span className="relative w-2 h-2 rounded-full bg-emerald-400">
                 <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
               </span>
-              Available for work
+              
             </div>
           </div>
           <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-x-[15ch] mt-5 mb-8 lg:mb-10">
